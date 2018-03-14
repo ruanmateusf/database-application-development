933b90acd7ea99de30bc5380e79610f7e29dcd6c
Ruan Mateus Francelino da Silva <ruanmateusf@gmail.com>
2018-03-13 21:57:14 -0300

    IF em PL/SQL

--- aula-05/exercicios.sql
+++ aula-05/exercicios.sql
@@ -1,1 +1,11 @@
+set serveroutput on
 
+declare 
+    v_idade number := &idade;
+begin
+    if v_idade < 18 then 
+        dbms_output.put_line ('Menor de idade');
+    else 
+        dbms_output.put_line ('Maior de idade');
+    end if;
+end;
