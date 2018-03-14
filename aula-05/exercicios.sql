31071dd90848201b2bf01684b8cfb5cb62ee6f4e
Ruan Mateus Francelino da Silva <ruanmateusf@gmail.com>
2018-03-13 22:24:13 -0300

    Update exercicios.sql

--- aula-05/exercicios.sql
+++ aula-05/exercicios.sql
@@ -9,3 +9,19 @@ begin
         dbms_output.put_line ('Maior de idade');
     end if;
 end;
+
+declare
+    v_cont number:=0;
+begin
+    loop
+        v_cont:=v_cont+1;
+        dbms_output.put_line (v_cont);
+        exit when v_cont=20;
+    end loop;
+end;
+
+begin
+    for x in 1..20 loop
+        dbms_output.put_line (x);
+    end loop;
+end;
